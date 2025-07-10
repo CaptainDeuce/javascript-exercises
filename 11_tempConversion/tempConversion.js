61686d9725c07d39dbe801adc03f34963945cdcd
Ethan Boes <deuceboes@gmail.com>
2025-07-10 15:06:17 -0400

    Round temperature to one decimal place

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,5 +1,6 @@
 const convertToCelsius = function(temp) {
-  return (temp - 32) * (5 / 9);
+  let stringCelciusTemp = ((temp - 32) * (5 / 9)).toFixed(1);
+  return parseFloat(stringCelciusTemp);
 };
 
 const convertToFahrenheit = function() {
