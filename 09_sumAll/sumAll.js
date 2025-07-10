eaca65e27fdd8dca4fab083143d0b072371facb4
Ethan Boes <deuceboes@gmail.com>
2025-07-10 14:46:02 -0400

    Add error check if negative or non-integer

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    if (num1 <= num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } else if (num1 <= num2) {
         for (let i = num1; i <= num2; i++) {
             sum += i;
         }
