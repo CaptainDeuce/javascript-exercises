d915950b1afa3b02538169f2967ebf66142a53d3
Ethan Boes <deuceboes@gmail.com>
2025-07-15 17:55:57 -0400

    Create findTheOldest function using a loop, not a js method

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // let currentOldest = 0;
+    // let oldest = arr.reduce((item) => {
+    //     if ((item.yearOfDeath - item.yearOfBirth) < currentOldest) {
+    //         return currentOldest;
+    //     }
+    // }, currentOldest);
 
+    let oldest = {};
+    let currentAge = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        if (age >= currentAge) {
+            currentAge = age;
+            oldest = arr[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
