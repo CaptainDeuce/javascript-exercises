35f659d38a41b67ba1e8def0cd506090d3df4874
Ethan Boes <deuceboes@gmail.com>
2025-07-15 17:42:52 -0400

    Make the getTheTitles function using the js map method

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,9 +1,11 @@
 const getTheTitles = function(arr) {
-    let titleArr = [];
-    for (let item of arr) {
-        titleArr.push(item.title);
-    }
-    return titleArr;
+    // let titleArr = [];
+    // for (let item of arr) {
+    //     titleArr.push(item.title);
+    // }
+    // return titleArr;
+
+    return arr.map(item => item.title);
 };
 
 // Do not edit below this line
