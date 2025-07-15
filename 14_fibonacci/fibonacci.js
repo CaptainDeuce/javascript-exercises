18d7e8eb15f84600175bbc1cadaf16a85092d4a0
Ethan Boes <deuceboes@gmail.com>
2025-07-14 20:56:43 -0400

    Fix the function to allow strings

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,11 +1,12 @@
 const fibonacci = function(number) {
+    let actualNumber = Number(number);
     let fibNumber = 0;
-    if (number === 1 || number === 2) {return 1}
-    if (number < 0) {return "OOPS"}
+    if (actualNumber === 1 || actualNumber === 2) {return 1}
+    if (actualNumber < 0) {return "OOPS"}
 
     let current = 1;
     let previous = 1;
-    for (let i = 3; i <= number; i++) {
+    for (let i = 3; i <= actualNumber; i++) {
         fibNumber = current + previous;
         previous = current;
         current = fibNumber;
