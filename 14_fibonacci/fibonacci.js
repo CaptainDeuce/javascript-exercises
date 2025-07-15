d9d7a57230f3dc0eb2dae8176c6ea43847b8a4dc
Ethan Boes <deuceboes@gmail.com>
2025-07-14 20:29:26 -0400

    Create fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fibNumber = 1;
+    if (number === 1 || number === 2) {return fibNumber}
+    for (let i = 1; i < number; i++) {
+        let j = 0;
+        fibNumber = i + j;
+        j++;
+    }
+    return fibNumber;
 };
 
 // Do not edit below this line
